36e39cea39fbb1f4d766a293592367d89df77573
Ben Buxton <bb@cactii.net>
2021-02-09 16:38:02 +1100

    Support for AM43 BLE blind/shade motor

---
+++ esphome/components/am43_cover/am43_cover.h
@@ -0,0 +1,48 @@
+#pragma once
+
+#include "esphome/core/component.h"
+#include "esphome/components/ble_client/ble_client.h"
+#include "esphome/components/esp32_ble_tracker/esp32_ble_tracker.h"
+#include "esphome/components/cover/cover.h"
+#include "am43_base.h"
+
+#ifdef ARDUINO_ARCH_ESP32
+
+#include <esp_gattc_api.h>
+
+namespace espbt = esphome::esp32_ble_tracker;
+
+namespace esphome {
+namespace am43_cover {
+
+#define AM43_SERVICE_UUID 0xFE50
+#define AM43_CHARACTERISTIC_UUID 0xFE51
+
+class Am43Cover : public cover::Cover, public esphome::ble_client::BLEClientNode, public Component {
+ public:
+  void setup() override;
+  void loop() override;
+//  void update() override;
+  void gattc_event_handler(esp_gattc_cb_event_t event, esp_gatt_if_t gattc_if,
+                           esp_ble_gattc_cb_param_t *param) override;
+  void dump_config() override;
+  float get_setup_priority() const override { return setup_priority::DATA; }
+  cover::CoverTraits get_traits() override;
+  void set_pin(uint16_t pin) { this->pin_ = pin; }
+
+  protected:
+  void control(const cover::CoverCall &call) override;
+  uint16_t char_handle_;
+  uint16_t pin_;
+  Am43Encoder *encoder_;
+  Am43Decoder *decoder_;
+  bool logged_in_;
+
+  float position_;
+};
+
+}  // namespace am43_cover
+}  // namespace esphome
+
+#endif
+
